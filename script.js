4f06b80f325f42996416f8a6db5479fcd0ffe018
Júlia de O. M. <julia.martins25@escola.pr.gov.br>
2026-09-10 15:27:13 +0000

    v1

---
+++ script.js
@@ -0,0 +1,5 @@
+const btnPromocao = document.querySelector("#btn_promocao");
+
+btnPromocao.addEventListner("click", function() {
+    alert("As reservas realizadas no dia de hoje recebem uma promoção especial!");
+});
