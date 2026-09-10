693f7c46ed996bf3061bba42820e33794fd9bc80
Júlia de O. M. <julia.martins25@escola.pr.gov.br>
2026-09-10 16:43:56 +0000

    v2

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-const btnPromocao = document.querySelector("#btn_promocao");
+const btnPromocao = document.querySelector("#btn-promocao");
+const btnSaibaMais = document.querySelector("#btn-saiba-mais");
+const mensagemDestino = document.querySelector("#mensagem-destino");
 
-btnPromocao.addEventListner("click", function() {
+btnPromocao.addEventListener("click", function() {
     alert("As reservas realizadas no dia de hoje recebem uma promoção especial!");
+});
+
+btnSaibaMais.addEventListener("click", function(){
+    mensagemDestino.textContent = "Pacote de 7 dias com hospedagem em hotel 4 estrelas no centro de Paris. Inclui ingressos para Torre Eiffel e Museu do Louvre, com café da manhã incluso.";
 });
